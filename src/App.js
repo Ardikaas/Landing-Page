bed97385e3c633c4162cc5623ff1777c5711b493
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-12 12:59:56 +0700

    add react tamplate tamplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
