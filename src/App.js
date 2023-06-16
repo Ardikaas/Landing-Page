0e78299eff4a1a04f4ca1c7632323698b1775eb4
RidhoAbaaz <aaridho16@gmail.com>
2023-06-16 21:03:44 +0700

    make footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
+        <Footer/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
