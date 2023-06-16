4361855b8def10eb3553713c1bac1500964da1d7
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-16 19:59:51 +0700

    fix route and make not found page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Landing from './pages/Landing';
+import Notfound from './pages/Notfound';
 
 function App() {
-  return <Landing />
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
