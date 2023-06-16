ec13910fe422cde12db8d5b03c07ff16a49a7984
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-16 19:46:50 +0700

    route Landing page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import Landing from './pages/Landing';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Landing />
 }
 
 export default App;
