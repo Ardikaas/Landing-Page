593ecf5420029e94036764fdd07fc4dbf7917278
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-20 16:30:23 +0700

    fixing dropdown menu navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,22 @@
 import image from './assets/logo.png'
+import React, {useState, useEffect, useRef} from 'react'
+
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  let menuRef = useRef()
+  useEffect(() => {
+    let handler = (e) => {
+      if(!menuRef.current.contains(e.target)){
+        setOpen(false)
+        console.log(menuRef.current)
+      }
+    }
+    document.addEventListener("mousedown", handler)
+    return() => {
+      document.removeEventListener("mousedown", handler)
+    }
+  })
   return(
     <div className="navbar-con">
       <div className="normal">
@@ -15,20 +31,20 @@ const Navbar = () => {
           </div>
         </div>
         <div className="right">
-              <a href="*">Request Demo</a>
+          <a href="*">Request Demo</a>
         </div>
       </div>
       <div className="hamburg">
         <div className="logo-ham">
           <img src={image} alt=""></img>
-          <i class="fa-solid fa-bars"></i>
+          <i class="fa-solid fa-bars" onClick={() => {setOpen(!open)}}></i>
         </div>
-        <div className="lambang">
-          <div className="kebawah">
-            <a href="#about">About</a>
-            <a href="*">Help</a>
-            <a href="#features">Features</a>
-            <a href="*" className="beda">Request Demo</a>
+        <div className='lambang' ref={menuRef}>
+          <div className={`kebawah ${open? 'active' : 'inactive'}`}>
+          <a href="#about">About</a>
+          <a href="*">Help</a>
+          <a href="#features">Features</a>
+          <a href="*" className="beda">Request Demo</a>
           </div>
         </div>
       </div>
