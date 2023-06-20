59f33f30e8e71ddf827cc62ae1797ff54b182f5b
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-20 11:08:06 +0700

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,18 @@
 import image from './assets/logo.png'
 
 const Navbar = () => {
+  const btntoggle = document.querySelector('.resicon')
+  const btntoggleicon = document.querySelector('.resicon i')
+  const accor = document.querySelector('.accordion')
+
+  btntoggle.onclick = function(){
+    accor.classList.toggle('open')
+    const isOpen = accor.classList.contains('open')
+
+    btntoggleicon.classList = isOpen
+      ? 'fa-solid fa-xmark'
+      : 'fa-solid fa-bars'
+  }
   return(
     <div class="navbar-con">
       <div className="normal">
@@ -15,11 +27,18 @@ const Navbar = () => {
           </div>
         </div>
         <div className="right">
-          <a href="*">Request Demo</a>
         </div>
       </div>
       <div className="hamburg">
-
+        <div className="logo">
+          <img src={image} alt=""></img>
+        </div>
+        <div className="kebawah">
+            <a href="#about">About</a>
+            <a href="*">Help</a>
+            <a href="#features">Features</a>
+            <a href="*" className="beda">Request Demo</a>
+        </div>
       </div>
     </div>
   )
